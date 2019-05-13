a67bcd6e1d499e24c6978782881921dec33aa389
katyamsko <46845648+katyamsko@users.noreply.github.com>
2019-05-13 12:58:27 +0300

    готово

--- lot.php
+++ lot.php
@@ -38,9 +38,35 @@ if (!$link) {
             . 'ON r.lot_id = l.id '
             . 'WHERE l.id = ' . $id_lot;
 
-            if ($res = mysqli_query($link, $sql)) {
+            $res = mysqli_query($link, $sql);
+
+            if ($res) {
                 $lot_array = mysqli_fetch_all($res, MYSQLI_ASSOC);
                 $lot = $lot_array[0];
+
+                $sql_table = 'SELECT r.rate_time, r.user_id, r.price as offer, r.lot_id, u.name as author_name FROM rate as r'
+                        . ' JOIN user as u'
+                        . ' ON r.user_id = u.id'
+                        . ' WHERE lot_id = ' . $id_lot
+                        . ' ORDER BY rate_time DESC'
+                        . ' LIMIT 10';
+                $res_table = mysqli_query($link, $sql_table);
+
+
+                $table_rates_array = mysqli_fetch_all($res_table, MYSQLI_ASSOC);
+                $table_rates = $table_rates_array;
+                $count = count($table_rates);
+
+                foreach ($table_rates as $key => $value) {
+                    $now = 'now';
+                    $time_rate = $value['rate_time'];
+
+                    $diff = ceil((-strtotime($time_rate) + strtotime($now)) / 60);
+                    $table_rates[$key]['rate_time_diff'] = $diff;
+                    $table_rates[$key]['time_diff_text'] = $diff . get_noun_plural_form($diff, ' минута ', ' минуты ', ' минут ') . 'назад';
+                }
+
+
 
                 if ($lot['current_price'] == null) {
                     $lot['current_price'] = $lot['start_price'];
@@ -58,23 +84,18 @@ if (!$link) {
                 if ($lot['lot_name']) {
 
                     if ($is_auth) {
-                        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                            $new_rate = $_POST;
 
-                            $required = ['cost'];
-                            $errors = [];
+                        $sql = 'SELECT * FROM rate'
+                            . ' WHERE lot_id = ' . $id_lot
+                            . ' ORDER BY price DESC'
+                            . ' LIMIT 1';
+                        $res = mysqli_query($link, $sql);
 
-                            foreach ($required as $value) {
-                                if (empty($new_rate[$value])) {
-                                    $errors[$value] = 'Заполните это поле';
-                                }
-                            }
+                        if ($res) {
+                            $lot_rates_array = mysqli_fetch_all($res, MYSQLI_ASSOC);
+                            $lot_rates = $lot_rates_array[0];
 
-                            if (!empty($new_rate['cost']) && (int)$new_rate['cost'] < $lot['minimal_price']) {
-                                $errors['cost'] = 'Цена должна быть не меньше минимальной ставки!';
-                            }
-
-                            if (count($errors)) {
+                            if ($lot_rates['user_id'] == $user_id) {
                                 $page_content = include_template('lot.php', [
                                     'categories' => $categories,
                                     'lot' => $lot,
@@ -82,33 +103,72 @@ if (!$link) {
                                     'showedTime' => $resultTime['time'],
                                     'class_item' => $resultTime['class'],
                                     'is_auth' => $is_auth,
-                                    'errors' => $errors,
-                                    'id_lot' => $id_lot
+                                    'user_flag' => true,
+                                    'table_rates' => $table_rates,
+                                    'count' => $count
                                 ]);
-                                $page_title = "Yeticave | Ошибка ставки";
+                                $page_title = $lot['lot_name'];
                             } else {
-                                $current_rate = $new_rate['cost'];
-                                $sql = 'INSERT INTO rate (rate_time, price, user_id, lot_id) VALUES (NOW(), ' . $current_rate . ', ' . $user_id . ', ' . $id_lot . ')';
-                                $res = mysqli_query($link, $sql);
-                                if ($res) {
-                                    header("Location: /lot.php?id=" . $id_lot);
-                                    exit();
+                                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                    $new_rate = $_POST;
+
+                                    $required = ['cost'];
+                                    $errors = [];
+
+                                    foreach ($required as $value) {
+                                        if (empty($new_rate[$value])) {
+                                            $errors[$value] = 'Заполните это поле';
+                                        }
+                                    }
+
+                                    if (!empty($new_rate['cost']) && (int)$new_rate['cost'] < $lot['minimal_price']) {
+                                        $errors['cost'] = 'Цена должна быть не меньше минимальной ставки!';
+                                    }
+
+                                    if (count($errors)) {
+                                        $page_content = include_template('lot.php', [
+                                            'categories' => $categories,
+                                            'lot' => $lot,
+                                            'price' => $price,
+                                            'showedTime' => $resultTime['time'],
+                                            'class_item' => $resultTime['class'],
+                                            'is_auth' => $is_auth,
+                                            'errors' => $errors,
+                                            'id_lot' => $id_lot,
+                                            'table_rates' => $table_rates,
+                                            'count' => $count
+                                        ]);
+                                        $page_title = "Yeticave | Ошибка ставки";
+                                    } else {
+                                        $current_rate = $new_rate['cost'];
+                                        $sql = 'INSERT INTO rate (rate_time, price, user_id, lot_id) VALUES (NOW(), ' . $current_rate . ', ' . $user_id . ', ' . $id_lot . ')';
+                                        $res = mysqli_query($link, $sql);
+                                        if ($res) {
+                                            header("Location: /lot.php?id=" . $id_lot);
+                                            exit();
+                                        } else {
+                                            $error = mysqli_error($link);
+                                            $page_content = include_template('error.php', ['error' => $error]);
+                                        }
+                                    }
                                 } else {
-                                    $error = mysqli_error($link);
-                                    $page_content = include_template('error.php', ['error' => $error]);
+                                    $page_content = include_template('lot.php', [
+                                            'categories' => $categories,
+                                            'lot' => $lot,
+                                            'price' => $price,
+                                            'showedTime' => $resultTime['time'],
+                                            'class_item' => $resultTime['class'],
+                                            'is_auth' => $is_auth,
+                                            'id_lot' => $id_lot,
+                                            'table_rates' => $table_rates,
+                                            'count' => $count
+                                        ]);
+                                        $page_title = $lot['lot_name'];
                                 }
                             }
                         } else {
-                            $page_content = include_template('lot.php', [
-                                    'categories' => $categories,
-                                    'lot' => $lot,
-                                    'price' => $price,
-                                    'showedTime' => $resultTime['time'],
-                                    'class_item' => $resultTime['class'],
-                                    'is_auth' => $is_auth,
-                                    'id_lot' => $id_lot
-                                ]);
-                                $page_title = $lot['lot_name'];
+                            $error = mysqli_error($link);
+                            $page_content = include_template('error.php', ['error' => $error]);
                         }
                     } else {
                         $page_content = include_template('lot.php', [
@@ -117,7 +177,9 @@ if (!$link) {
                             'price' => $price,
                             'showedTime' => $resultTime['time'],
                             'class_item' => $resultTime['class'],
-                            'is_auth' => $is_auth
+                            'is_auth' => $is_auth,
+                            'table_rates' => $table_rates,
+                            'count' => $count
                         ]);
                         $page_title = $lot['lot_name'];
                     }
--- templates/lot.php
+++ templates/lot.php
@@ -19,7 +19,7 @@
           <p class="lot-item__description"><?=$lot['description']; ?> </p>
         </div>
         <div class="lot-item__right">
-        <?php if ($is_auth): ?>
+        <?php if ($is_auth && !$user_flag): ?>
           <div class="lot-item__state">
             <div class="lot-item__timer timer <?=$class_item; ?>">
               <?=$showedTime; ?>
@@ -57,58 +57,18 @@
           </div>
         <?php endif; ?>
           <div class="history">
-            <h3>История ставок (<span>10</span>)</h3>
+            <h3>История ставок (<span><?=$count; ?></span>)</h3>
             <table class="history__list">
+
+              <?php foreach ($table_rates as $key => $value) :?>
+
               <tr class="history__item">
-                <td class="history__name">Иван</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">5 минут назад</td>
+                <td class="history__name"><?=$value['author_name']; ?></td>
+                <td class="history__price"><?=$value['offer']; ?></td>
+                <td class="history__time"><?=$value['time_diff_text']; ?></td>
               </tr>
-              <tr class="history__item">
-                <td class="history__name">Константин</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">20 минут назад</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Евгений</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">Час назад</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Игорь</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 08:21</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Енакентий</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 13:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Семён</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 12:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Илья</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 10:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Енакентий</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 13:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Семён</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 12:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Илья</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 10:20</td>
-              </tr>
+              <? endforeach; ?>
+
             </table>
           </div>
         </div>
